397eac586ade48a2ac2a25a7b248028530421fe5
davidmstanleyjr <davidmstanleyjr@gmail.com>
2022-04-24 18:07:52 -0400

    print exercises

--- examples/learning.py
+++ examples/learning.py
@@ -6,3 +6,24 @@ bool_val = False
 integer = 8
 
 print(integer)
+
+# this is a comment
+
+# print practice
+
+example_1 = 3.14159
+example_2 = 24601
+example_3 = True
+
+
+print(example_1);
+print(example_2);
+print(example_3)
+print (15 / 3 * 2 * 2 - (3 + 4))
+
+
+# exercise
+var1 = 25
+print(var1);
+print("chicken")
+print(2+5*3+2)
