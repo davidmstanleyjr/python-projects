9301f49b62ca7366a4787f7e72edbf20f7680d9a
davidmstanleyjr <davidmstanleyjr@gmail.com>
2022-06-27 14:55:14 -0400

    floats and types

--- examples/learning.py
+++ examples/learning.py
@@ -43,3 +43,22 @@ concatenated = "R2" + "-" + "D2"
 print(concatenated)
 print(concatenated[3])
 print(concatenated[1:4])
+
+# type(), str(), and escape sequences exercises
+# Do all of this in a .py file in Pycharm.
+
+# Create a variable and assign it a float
+
+# Use print() and type() to print the data type of the variable in the output
+
+# Use str() on the variable from step 1 and concatenate it with the string " is a float." then use print() to display the result
+
+# print() the string "Hello, I'm [name], it's nice to meet you!" including quotes (you will need to use the \' or \" escape sequence depending on whether you enclose your strings in single quotes or double quotes.)
+
+float_num = 8.5 
+
+print(type(float_num))
+
+print(str(float_num) + " is a float.")
+
+print("\"Hello, I'm David, it's nice to meet you!\"")
